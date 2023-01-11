3aeb2a589fca47db589409e2c1354b3cdae8f66f
devkamaljit <106373158+devkamaljit@users.noreply.github.com>
2023-01-11 11:28:37 +0530

    f commmit KML

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import RegisterPage from './Pages/RegisterPage/RegisterPage';
+import { useWeb3React } from "@web3-react/core";
+import { injected } from "./wallet/Connect"
+import web3 from "web3";
+const App = () => {
+
+  return (
+    <div>
+      <RegisterPage />
+
+    </div>
+  )
+}
+
+export default App
